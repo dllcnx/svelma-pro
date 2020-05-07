63408c85cfe80043423d2ffbf83927a401271032
KeiferJu <jkf19980216@qq.com>
2020-05-07 09:32:27 +0800

    调整

--- deploy.js
+++
@@ -1,9 +0,0 @@
-var ghpages = require('gh-pages');
-
-ghpages.publish('docs/__sapper__/export/svelma-pro', {
-  branch: 'gh-pages',
-  repo: 'https://github.com/KeiferJu/svelma-pro.git',
-  message: 'deploy',
-}, function (err) {
-  console.log(err);
-});
